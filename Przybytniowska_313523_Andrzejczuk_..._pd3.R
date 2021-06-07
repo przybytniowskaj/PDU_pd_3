fb61f6aab86166b53435a048095570936edb5660
andrzejczukm <72359059+andrzejczukm@users.noreply.github.com>
2021-06-07 21:52:47 +0200

    Update Przybytniowska_313523_Andrzejczuk_..._pd3.R

--- Przybytniowska_313523_Andrzejczuk_..._pd3.R
+++ Przybytniowska_313523_Andrzejczuk_..._pd3.R
@@ -284,3 +284,35 @@ server <- function(input, output) {
 
 shinyApp(ui = ui, server = server)
 
+##dodane
+library(shiny)
+library(knitr)
+library(markdown)
+
+ui <- shinyUI(
+  fluidPage(
+  fluidRow(
+    column(
+      width = 6,
+      tags$textarea(
+        id = "text",
+        cols = 50,
+        rows = 30
+      )
+    ),
+    column(
+      width = 6,
+      uiOutput('markdown')
+    )
+  )
+)
+)
+
+server <- function(input, output) {
+  output$markdown <- renderUI({
+    HTML(markdown::markdownToHTML(text = input$text))
+  })
+}
+
+shinyApp(ui, server)
+
